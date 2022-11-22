5016ac4ab029b4e425682412a25b54cf42ca481b
Максим Долженко <maxim_2003@mail.ru>
2022-11-22 17:35:35 +0300

    Update build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,23 @@
 plugins {
-    id("java")
+    java
 }
 
-group = "ru.mail"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "ru.mail"
+    version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+subprojects {
+    apply(plugin = "java")
+    dependencies {
+        implementation("org.jetbrains:annotations:20.1.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    }
 }
 
 tasks.getByName<Test>("test") {
